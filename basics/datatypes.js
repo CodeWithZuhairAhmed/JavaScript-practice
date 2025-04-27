118dba2c0d1fc5c39e2b7dc031d4a0aa1caf7dc3
Zuhair Ahmed <codewithzuhairahmed@gmail.com>
2025-04-27 09:46:37 +0000

    understanding about datatypes.

---
+++ basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //method to use strict version of javascript
+
+// alert(hello); this method is for browser not for node js.
+
+let username = "Zuhair"; //string datatype.
+let age = 18; //number datatype.
+let isLoggedin = true; //boolean datatype only true/false. 
+
+// number 2--> to power 53.
+// bigInt
+//string ""
+//boolean true/false
+//null --> standalone value and it is object
+// undefined --> it means that value is not defined
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
